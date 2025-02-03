520a95f0bb7c15a0cb3e64a46af36038b481edf7
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-03 09:54:05 -0700

    expression notes

--- practice.py/expression/note.py
+++ practice.py/expression/note.py
@@ -3,3 +3,11 @@
 #floats are number with decimal points 
 #algorithms are a set of instructions to complete a task 
 print(10+5)
+print(10.5-5)
+print(10.5*5)
+print(10.5/5)
+print(10.5**5) #exponents 
+print(10.5//5) #whole number division
+print(10.5//5) #modulo/modulus/MOD gives the remainder 
+
+print((3+7)*2)
---
+++ practice.py/expression/notes.C
@@ -0,0 +1,17 @@
+//andrea lugo, expressions notes C
+#include <studio.H
+//integres int whem we set the variable and %d whem we print
+//floats float whem we set the variable and %f when we print
+// strings (words) char whem we set the variable and whem we print
+int mynum;
+floart percent;
+
+int main(void){
+    printf("type a number: \n")
+    scanf("%d" , &mynum);
+    printf("you inputed %d" , mynum);
+    printf("give me a percent as a decimal: \n");
+    scanf("%f" , &percent);
+    prints("your percent is %f\n" , percent);
+    return 0
+} 
