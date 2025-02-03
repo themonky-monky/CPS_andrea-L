700aeebef3636d25c9bdec06e3ba9c76bc19d81e
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-03 09:29:51 -0700

    first_program.py

---
+++ practice.py/expression/note.py
@@ -0,0 +1,5 @@
+#andrea lugo, notes python
+#integers are numbers with no decimal ponts
+#floats are number with decimal points 
+#algorithms are a set of instructions to complete a task 
+print(10+5)
