ee922481dd9c8d796f5be2f015f9c12d48f152ec
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-06 09:49:30 -0700

    financial_calc.py

--- expression/financial_calc.py
+++ expression/financial_calc.py
@@ -1,1 +1,39 @@
-# andrea lugo, financial calculator python
+# Andrea lugo, financial calculator python
+
+# write a print statement telling the user what the program is (budget calculator)
+
+# Ask for monthly income (user input)
+income = float (input("what is your monthy income\n"))
+#Ask for rent amount (user input)
+income = float (input("what is your monthy amount\n"))
+#Ask for utilities amount (user input)
+income = float (input("what is your monthy utilities\n"))
+#Ask for groceries amount (user input)
+income = float (input("what is your monthy groseries\n"))
+#Ask for transportation amount (user input)
+income = float (input("what is your monthy transportation\n"))
+# calculation savings as 10% of income (variable)
+
+# calculation spending money income - (rent+utilities+groceries+transportation+savings) (variable)
+
+# calculation percent of rent (rent/income) (variable)
+
+# calculation percent of utilities (utilities/income) (variable)
+
+# calculation percent of groceries (groceries/income) (variable)
+
+# calculation percent of transportation (transportation/income) (variable)
+
+# tell user category spending amount AND percent for rent ("you spend $xx% on rent that is xx% of your income")
+
+# tell user category spending amount AND percent for utilities ("you spend $xx% on utilities that is xx% of your income")
+
+# tell user category spending amount AND percent for groceries ("you spend $xx% on groceries that is xx% of your income")
+
+
+# tell user category spending amount AND percent for transportation ("you spend $xx% on transportation that is xx% of your income")
+
+
+# tell user category spending amount AND percent for savings("you spend $xx% on savings that is xx% of your income")
+
+# tell user category spending amount AND percent for spending  ("you spend $xx% on spending that is xx% of your income")
