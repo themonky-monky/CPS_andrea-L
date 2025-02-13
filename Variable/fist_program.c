0a7083941ed6eeeafca8ff0f24b65ad6490fd683
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-13 23:27:30 +0000

    fist_program.c

--- Variable/fist_program.c
+++ Variable/fist_program.c
@@ -1,7 +1,15 @@
  //andrea lugo,first program 
  #include <stdio.h>
 
+ char name[30]; 
+
  int main(void){
+
     printf("Hello word\n");
+    printf("pleaser tell me your name/n");
+    scanf ("%", name);
+
+    printf("hello %s, welcome to my program", name);
+    printf("this is my first program\n"); 
     return 0; 
  } 
