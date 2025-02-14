7c6496bb9cb81aa1560cd86536c8eda7ebdd9a3f
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-14 23:52:38 +0000

    silly_sentence.c

---
+++ expression/string/silly_sentence.c
@@ -0,0 +1,23 @@
+//andrea lugo, silly sentence c
+#include <stdio.h>
+#include <string.h>
+
+char name[10];
+
+int main(void){
+    char gorilla[25], mall[10], shopping[20];
+
+    printf("Enter an gorilla: ");
+    scanf("%s25", gorilla);
+
+    printf("Enter a mall: ");
+    scanf("%s10", mall);
+
+    printf("Enter an action (shopping): ");
+    scanf("%s20", shopping);
+
+    printf("One day, a KingKong went to the Mall Of America and decided to shop all day long.\n", gorilla, mall, shopping);
+
+    
+    return 0;
+}
---
+++ expression/string/silly_sentence.py
@@ -0,0 +1,26 @@
+#andrea lugo, silly sentence.py
+
+#words in programing (data type)""
+
+#print("this is Andrea code")
+#nun = 5*2
+#print(num)
+#name = input("what is your name?\n").strip().capitalize()
+
+#print(f"hello{name} welcome to my program.")
+#print(name + "2")
+
+sentence = "the quick brown fot jumps over the lazy dog"
+
+word = sentence. find("fox")
+#print(sentence[4:9])
+#print(len(sentence))
+name = "lugo"
+print("welcome to my program"+name)
+
+lion = input("Enter an animal: ").strip()
+zoo = input("Enter a place: ").strip()
+dance = input("Enter an action (verb): ").strip()
+
+print(f"One day, a {lion} went to the {zoo} and decided to {dance} all day long.")
+
