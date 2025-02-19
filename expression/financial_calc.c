782a413a19a2570e63bae6185674d7e4dd041aa0
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-19 00:39:35 +0000

    financial_calc.c

--- expression/financial_calc.c
+++ expression/financial_calc.c
@@ -1,4 +1,4 @@
-//andrea lugo, financial_calc
+//andrea lugo, financial_calc.c
     #include <stdio.h>   
     
 int main(void){
@@ -8,7 +8,7 @@ float prent, puntilities, pgroceries, ptransportation, psaving, pexpenses;
 printf("Hello and welcome to our financial calculator.\n");  
 printf("How much do you make each month? ");  
 scanf("%f", &income);  
-printf("Your monthly income is \$%.2f\n", income);   
+printf("Your monthly income is $%.2f\n", income);   
 printf("How much is your rent? ");  
 scanf("%f", &rent);   
 printf("How much are your utilities? ");  
@@ -20,9 +20,9 @@ scanf("%f", &transportation);
 expenses = rent + utilities + groceries + transportation;  
 saving = income * 0.2;   
  total = income - expenses - saving;  
-printf("Your monthly expenses are \$%.2f\n", expenses);  
-printf("Your monthly savings are \$%.2f\n", saving);  
-printf("You have \$%.2f remaining after expenses and savings.\n", total);  
+printf("Your monthly expenses are $%.2f\n", expenses);  
+printf("Your monthly savings are $%.2f\n", saving);  
+printf("You have $%.2f remaining after expenses and savings.\n", total);  
 prent = (rent / income) * 100;  
 puntilities = (utilities / income) * 100;  
 pgroceries = (groceries / income) * 100;  
@@ -37,4 +37,4 @@ printf("Your expenses are %.1f%% of your income.\n", pexpenses);
 printf("Your savings are %.1f%% of your income.\n", psaving);  
 
 return 0; 
-{
+}
