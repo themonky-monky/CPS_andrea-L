6701118bc970be836f1389b378c2b4a4d5424e5d
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-20 19:22:54 +0000

    financial_calc.py

--- expression/financial_calc.py
+++ expression/financial_calc.py
@@ -15,30 +15,30 @@ transportation = float(input("what is your monthy transportation? $"))
 # calculation savings as 10% of income (variable)
 saving = income *0.10
 # calculation spending money income - (rent+utilities+groceries+transportation+savings) (variable)
-spending_money = ("rent + utilities + groceries + transportation + savings")
+spending = ("rent + utilities + groceries + transportation + savings")
 # Calculate spending money as income minus (expenses + savings)
-spending_money = income - (saving)
+spending= income - (saving)
 # calculation percent of rent (rent/income) (variable)
-rent_percent = (rent/income)*100
+rent_percent = rent/income*100
 # calculation ercent of utilities (utilities/income) (variable)
-utilities_percent = (utilities/income)*100
+utilities_percent = utilities/income*100
 # calculation percent of groceries (groceries/income) (variable)
-groceries_percent = (groceries/income)*100 
-# calculation percent of transportation (transportation/income) (variable)
-transportation_percent = (transportation/income)*100
+groceries_percent= groceries/income*100 
+# calculatio percent of transportation (transportation/income) (variable)
+transportation_percent = transportation/income*100
 #calculator percet of saving (saving/income) (variable)
 saving_percent = (saving/income)*100
 #calculator percent of speding (speding/income) (variable)
-spending_money_percent = (spending_money/income)*100
+spending_percent = (spending/income)*100
 # tell user category spending amount AND percent for rent 100 ("you spend $xx% on rent that is xx% of your income")
-print(f"You spend ${rent} on rent, which is (rent_percent:.2f)% of your income")
+print(f"You spend ${rent} on rent, which is {rent_percent:.2}% of your income")
 # tell user category spending amount AND percent for utilities 100 ("you spend $xx% on utilities that is xx% of your income")
-print(f"You spend ${utilities} on utilities, which is (utilities_percent:.2f)% of your income")
+print(f"You spend ${utilities} on utilities, which is {utilities_percent:.2f}% of your income")
 # tell user category spending amount AND percent for groceries 100 ("you spend $xx% on groceries that is xx% of your income")
-print(f"You spend ${groceries} on groceries, which is (groceries_percent:.2f)% of your income")
+print(f"You spend ${groceries} on groceries, which is {groceries_percent:.2f}% of your income")
 # tell user category spending amount AND percent for transportation 100( "you spend $xx% on transportation that is xx% of your income")
-print(f"You spend ${transportation} on transportation, which is (transportation_percent:.2f)% of your income")
+print(f"You spend ${transportation} on transportation, which is {transportation_percent:.2f}% of your income")
 # tell user category spending amount AND percent for savings 100 ("you spend $xx% on savings that is xx% of your income")
-print(f"You save ${saving} each month, which is (savings_percent:.2f)% of your income")
+print(f"You save ${saving} each month, which is {saving_percent:.2f}% of your income")
 # tell user category spending amount AND percent for spending  100 ("you spend $xx% on spending that is xx% of your income")
-print(f"You have ${spending_money}remaining for other spending, which is (spending_percent:.2f)% of your income") 
+print(f"You have ${spending}remaining for other spending, which is {spending_percent:.2f}% of your income") 
