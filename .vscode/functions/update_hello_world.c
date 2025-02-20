d2938efec8e9d49d93ff895720f619b6f7ebf90d
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-20 23:39:12 +0000

    update_hello_world.c

---
+++ .vscode/functions/update_hello_world.c
@@ -0,0 +1,15 @@
+//andrea lugo, update_hello_world.c
+#include <stdio.h>
+
+void info(char name[]){
+    printf("hello, %s!\n", name);
+
+}
+int main(void){
+    info("andrea");
+    info("lucas");
+    info("carlos");
+    info("alejandro");
+    info("rocio");
+    return 0; 
+}
