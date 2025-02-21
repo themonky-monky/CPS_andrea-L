1345ce28bddd12530a682345ad9ecb93be4d81de
themonky-monky <andrea.lugo@ucas-edu.net>
2025-02-21 00:11:48 +0000

    expressionspractice.c

--- expression/expressionspractice.c
+++ expression/expressionspractice.c
@@ -1,27 +1,28 @@
   //andrea lugo expression practice
   #include <stdio.h>
-#include <math.h>
-int main(void){
+  #include <math.h>
 
-int 7-24/8*4+6;
-int 18/3-7+2*5;
-int 6*4/12+72/8-9;
-int(17-6/2)+4*3;
-int -2(1*4-2/2)+(6+2-3);
-int-1*(3-4*7)/5 -2*24/6;
-int(3*5^2/15)-(5-2^2);
-int(1^4*2^2+3^3)-2^5/4;
-int(22/2-2.5)^2+(4-6/6)^2;
+  int main(void) {
 
-printf("%d\n,equation1");
-printf("%d\n,equation2");
-printf("%d\n,equation3");
-printf("%d\n,equation4");
-printf("%d\n,equation5");
-printf("%d\n,equation6");
-printf("%d\n,equation7");
-printf("%d\n,equation8");
-printf("%d\n,equation9");
+    int equation1 = 7 - 24 / 8 * 4 + 6;
+    int equation2 = 18 / 3 - 7 + 2 * 5;
+    int equation3 = 6 * 4 / 12 + 72 / 8 - 9;
+    int equation4 = (17 - 6 / 2) + 4 * 3;
+    int equation5 = -2 * (1 * 4 - 2 / 2) + (6 + 2 - 3);
+    int equation6 = -1 * (3 - 4 * 7) / 5 - 2 * 24 / 6;
+    int equation7 = (3 * pow(5, 2) / 15) - (5 - pow(2, 2));
+    int equation8 = (pow(1, 4) * pow(2, 2) + pow(3, 3)) - pow(2, 5) / 4;
+    int equation9 = pow((22 / 2 - 2 * 5), 2) + pow((4 - 6 / 6), 2);
 
-return 0;
+    printf("Equation 1: %d\n", equation1);
+    printf("Equation 2: %d\n", equation2);
+    printf("Equation 3: %d\n", equation3);
+    printf("Equation 4: %d\n", equation4);
+    printf("Equation 5: %d\n", equation5);
+    printf("Equation 6: %d\n", equation6);
+    printf("Equation 7: %d\n", equation7);
+    printf("Equation 8: %d\n", equation8);
+    printf("Equation 9: %d\n", equation9);
+
+    return 0;
 }
