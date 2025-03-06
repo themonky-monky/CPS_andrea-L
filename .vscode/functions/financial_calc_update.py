4373df020c6c47e817e5e99b74ddac64f724d979
themonky-monky <andrea.lugo@ucas-edu.net>
2025-03-06 16:49:24 +0000

    financial_calc_update.py

--- .vscode/functions/financial_calc_update.py
+++ .vscode/functions/financial_calc_update.py
@@ -4,31 +4,37 @@
 
 print("hello and welcome to your financial pcalculator\n")
 # Ask for monthly income (user input)
-income = float (input("what is your monthy income"))
+pincome = float (input("what is your monthly income\n"))
 #Ask for rent amount (user input)
-rent = float (input("what is your monthy rent\n"))
+prent = float (input("what is your monthly rent\n"))
 #Ask for utilities amount (user input)
-utilities = float (input("what is your monthy utilities\n"))
+putilities = float (input("what is your monthly utilities\n"))
 #Ask for groceries amount (user input)
-groceries = float (input("what is your monthy groseries\n"))
+pgroceries = float (input("what is your monthly groceries\n"))
 #Ask for transportation amount (user input)
-transportation = float (input("what is your monthy transportation\n"))
-saving = income * 0.2
-expenses = rent + utilities + groceries + transportation + saving
-spending = income - expenses
+ptransportation = float (input("what is your monthly transportation\n"))
+# calculator saving as 20% of income (variable)
+psaving = pincome * 0.20
+# calculator spending money income - (rent+utilities+groceries+transportation+savings) (variable)
+pexpenses = prent + putilities + pgroceries + ptransportation + psaving
+pspending = pincome - pexpenses
 
 
 def percent(type, amount):
-    percentage = amount / income * 100
+    percentage = amount / pincome * 100
     return f"you spend \${amount:.2f} on {type}, which is {percentage:.2f}% of your income"
 
-print(f"Your monthly income is ${income:.2f}\n")
-print(f"Your total monthly expenses are ${expenses:.2f}\n")
-print(f"Your monthly savings are ${saving:.2f}\n")
-print(f"Your monthly spending money is ${spending:.2f}\n")
-print(percent("rent", rent))
-print(percent("utilities", utilities))
-print(percent("groceries", groceries))
-print(percent("transportation", transportation))
-print(percent("saving", saving))
-print(percent("expenses", expenses)) 
+print(f"Your monthly income is ${pincome:.2f}\n")
+print(f"Your total monthly expenses are ${pexpenses:.2f}\n")
+print(f"Your monthly savings are ${psaving:.2f}\n")
+print(f"Your monthly spending money is ${pspending:.2f}\n")
+print(f"your monthly rent is ${prent:.2f}\n")
+print(f"your monthly utilities are ${putilities:.2f}\n")
+print(f"your monthly groceries are ${pgroceries:.2f}\n")
+print(f"your monthly transportation is ${ptransportation:.2f}\n")
+print(percent("rent", prent))
+print(percent("utilities", putilities))
+print(percent("groceries", pgroceries))
+print(percent("transportation", ptransportation))
+print(percent("savings", psaving))
+print(percent("spending money", pspending))
