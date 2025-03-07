30d4fe7c7f6bee2003ae1ed892914b027bb3ac16
themonky-monky <andrea.lugo@ucas-edu.net>
2025-03-07 00:08:02 +0000

    financial_calc_update.py

--- .vscode/functions/financial_calc_update.py
+++ .vscode/functions/financial_calc_update.py
@@ -1,8 +1,9 @@
 # Andrea lugo, financial calculator python program
 
+def budget_calculator():
 # write a print statement telling the user what the program is (budget calculator)
 
-print("hello and welcome to your financial pcalculator\n")
+    print("hello and welcome to your financial pcalculator\n")
 # Ask for monthly income (user input)
 pincome = float (input("what is your monthly income\n"))
 #Ask for rent amount (user input)
@@ -32,9 +33,12 @@ print(f"your monthly rent is ${prent:.2f}\n")
 print(f"your monthly utilities are ${putilities:.2f}\n")
 print(f"your monthly groceries are ${pgroceries:.2f}\n")
 print(f"your monthly transportation is ${ptransportation:.2f}\n")
+
 print(percent("rent", prent))
 print(percent("utilities", putilities))
 print(percent("groceries", pgroceries))
 print(percent("transportation", ptransportation))
 print(percent("savings", psaving))
 print(percent("spending money", pspending))
+
+budget_calculator()
