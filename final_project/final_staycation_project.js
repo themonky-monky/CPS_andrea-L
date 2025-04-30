eaa88e2dd9ea3639a22e9e92f5b54658be34fd1a
themonky-monky <andrea.lugo@ucas-edu.net>
2025-04-29 18:52:50 -0600

    Update final_staycation_project.js

--- final_project/final_staycation_project.js
+++ final_project/final_staycation_project.js
@@ -1,1 +1,9 @@
-let images = [
+ // JavaScript to toggle the visibility of additional content
+        function toggleContent() {
+            const content = document.getElementById('extra-content');
+            if (content.style.display === 'none') {
+                content.style.display = 'block';
+            } else {
+                content.style.display = 'none';
+            }
+        }
